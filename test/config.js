dc08cc69e8adadf4865168734d41ecf9ab4fdaa8
Marek Kurdej <marek@quasardb.net>
2017-06-28 14:51:02 +0200

    Case 1616 - [security] Disable security in tests.

--- test/config.js
+++ test/config.js
@@ -1,7 +1,7 @@
 var address = "127.0.0.1:3030";
 
 module.exports = {
-    "qdbd_path": __dirname + '/../deps/qdb/bin/qdbd',
-    "qdbd_args": [ '--address='+address, '--transient' ],
-    "cluster_uri": "qdb://" + address
+    "qdbd_path" : __dirname + '/../deps/qdb/bin/qdbd',
+    "qdbd_args" : [ '--security=false', '--address=' + address, '--transient' ],
+    "cluster_uri" : "qdb://" + address
 };
