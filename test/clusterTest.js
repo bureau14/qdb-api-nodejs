7b418478116f259af01163e3bcecfdebda46fd1f
Rohit <rohit.agarwal2014@gmail.com>
2017-12-15 13:55:19 +0100

    Case 1831 - [node] expose get_timeout function in nodejs (#10)
    * Added get_timeout option.
    
    * Formatting
    
    * Grammar error fix.

--- test/clusterTest.js
+++ test/clusterTest.js
@@ -1,6 +1,6 @@
 var test = require('unit.js');
 var qdb = require('..');
-var config = require('./config')
+var config = require('./config');
 
 describe('Cluster', function() {
     var cluster = new qdb.Cluster(config.cluster_uri);
@@ -38,7 +38,7 @@ describe('Cluster', function() {
     }); // connect
 
     describe('setTimeout', function() {
-        it('should set the timeout to 5\'000 ms', function() {
+        it('should set the timeout to 5 seconds', function() {
             cluster.setTimeout(5000);
         });
 
@@ -48,4 +48,16 @@ describe('Cluster', function() {
             });
         });
     }); // setTimeout
+
+    describe('getTimeout', function() {
+        it('should return the default timeout of 5 seconds', function(done) {
+            test.must(cluster.getTimeout()).be.equal(5000);
+            done();
+        });
+        it('should return the new timeout after changing the default timeout', function(done) {
+            cluster.setTimeout(3000);
+            test.must(cluster.getTimeout()).be.equal(3000);
+            done();
+        });
+    }); // getTimeout
 });
