d99d033778d4c9a892a6ea1e719eee834b2745b1
Marek Kurdej <marek@quasardb.net>
2017-12-20 14:34:58 +0100

    [release] Full version with tags.

--- scripts/release/set_version.sh
+++ scripts/release/set_version.sh
@@ -17,8 +17,20 @@ PATCH_VERSION=${WITHOUT_MINOR_VERSION%%.*}
 
 XYZ_VERSION="${MAJOR_VERSION}.${MINOR_VERSION}.${PATCH_VERSION}"
 
+if [[ "${INPUT_VERSION}" == *-* ]] ; then
+    TAGS_VERSION=${INPUT_VERSION#*-}
+else
+    TAGS_VERSION=
+fi
+
+if [[ -n "${TAGS_VERSION}" ]] ; then
+    FULL_XYZ_VERSION="${XYZ_VERSION}-${TAGS_VERSION}"
+else
+    FULL_XYZ_VERSION="${XYZ_VERSION}"
+fi
+
 cd $(dirname -- $0)
 cd ${PWD}/../..
 
 # "version": "2.1.0-nightly.5",
-sed -i -e 's/"version": "[^"]*"/"version": "'"${XYZ_VERSION}"'"/' package.json
+sed -i -e 's/"version": "[^"]*"/"version": "'"${FULL_XYZ_VERSION}"'"/' package.json
