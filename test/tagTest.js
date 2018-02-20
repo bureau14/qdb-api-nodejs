a5b29deecb4e95525c6bfb9ebef933501fc0368c
vianney <vianney.plotton@gmail.com>
2018-02-20 17:06:42 +0100

    Case 1904 - Solve sporadically failing tests.

--- test/tagTest.js
+++ test/tagTest.js
@@ -5,42 +5,42 @@ var config = require('./config')
 var cluster = new qdb.Cluster(config.cluster_uri);
 
 
-describe('Tag', function() {
+describe('Tag', function () {
     var dasTag = 'u96';
     var t = null;
 
-    before('connect', function(done) {
+    before('connect', function (done) {
         cluster.connect(done, done);
     });
 
-    before('init', function() {
+    before('init', function () {
         t = cluster.tag(dasTag);
     });
 
-    it('should be of correct type', function() {
+    it('should be of correct type', function () {
         test.object(t).isInstanceOf(qdb.Tag);
     });
 
-    it("should have an 'alias' property", function() {
+    it("should have an 'alias' property", function () {
         test.object(t).hasProperty('alias');
         test.must(t.alias()).be.a.string();
         test.must(t.alias()).be.equal(dasTag);
     });
 
-    describe('entries list test', function() {
+    describe('entries list test', function () {
         var b, i, q, s, ts;
 
-        before('init', function() {
+        before('init', function (done) {
             b = cluster.blob('blob_tag_test');
             i = cluster.integer('int_tag_test');
             q = cluster.deque('deque_tag_test');
             s = cluster.set('set_tag_test');
             ts = cluster.ts('time_series_tag_test');
+            done()
         });
 
-        it('should return an empty tag list', function(done) {
-            t.getEntries(function(err, entries)
-            {
+        it('should return an empty tag list', function (done) {
+            t.getEntries(function (err, entries) {
                 test.must(err).be.equal(null);
 
                 entries.must.have.length(0);
@@ -49,99 +49,80 @@ describe('Tag', function() {
             });
         });
 
-        it('should create the blob for tag test', function(done)
-        {
-            b.put(new Buffer('untz'), function(err)
-            {
+        it('should create the blob for tag test', function (done) {
+            b.put(new Buffer('untz'), function (err) {
                 test.must(err).be.equal(null);
 
                 done();
             });
         });
 
-        it('should create the integer for tag test', function(done)
-        {
-            i.update(0, function(err)
-            {
+        it('should create the integer for tag test', function (done) {
+            i.update(0, function (err) {
                 test.must(err).be.equal(null);
 
                 done();
             });
         });
 
-        it('should create the deque for tag test', function(done)
-        {
-            q.pushBack(new Buffer('mmm ok'), function(err)
-            {
+        it('should create the deque for tag test', function (done) {
+            q.pushBack(new Buffer('mmm ok'), function (err) {
                 test.must(err).be.equal(null);
 
                 done();
             });
         });
 
-        it('should create the set for tag test', function(done)
-        {
-            s.insert(new Buffer('b00m'), function(err)
-            {
+        it('should create the set for tag test', function (done) {
+            s.insert(new Buffer('b00m'), function (err) {
                 test.must(err).be.equal(null);
 
                 done();
             });
         });
 
-        it('should create the time series double columns for tag test', function(done)
-        {
-			ts.create([qdb.DoubleColumnInfo("col1")], function(err) {
-				test.must(err).be.equal(null);
-
-				done();
-			});
-        });
-
-        it('should tag the blob successfully', function(done)
-        {
-            b.attachTag(dasTag, function(err)
-            {
+        it('should create the time series double columns for tag test', function (done) {
+            ts.create([qdb.DoubleColumnInfo("col1")], function (err) {
                 test.must(err).be.equal(null);
 
                 done();
             });
         });
 
-        it('should tag the integer successfully', function(done)
-        {
-            i.attachTag(dasTag, function(err)
-            {
+        it('should tag the blob successfully', function (done) {
+            b.attachTag(dasTag, function (err) {
                 test.must(err).be.equal(null);
 
                 done();
             });
         });
 
-        it('should tag the deque successfully', function(done)
-        {
-            q.attachTag(dasTag, function(err)
-            {
+        it('should tag the integer successfully', function (done) {
+            i.attachTag(dasTag, function (err) {
                 test.must(err).be.equal(null);
 
                 done();
             });
         });
 
-        it('should tag the set successfully', function(done)
-        {
-            s.attachTag(dasTag, function(err)
-            {
+        it('should tag the deque successfully', function (done) {
+            q.attachTag(dasTag, function (err) {
                 test.must(err).be.equal(null);
 
                 done();
             });
         });
 
-        it('should tag the time series successfully', function(done)
-        {
-            ts.attachTag(dasTag, function(err)
-            {
+        it('should tag the set successfully', function (done) {
+            s.attachTag(dasTag, function (err) {
+                test.must(err).be.equal(null);
+
+                done();
+            });
+        });
+
+        it('should tag the time series successfully', function (done) {
+            ts.attachTag(dasTag, function (err) {
                 test.must(err).be.equal(null);
 
                 done();
@@ -158,10 +139,8 @@ describe('Tag', function() {
             });
         });
 
-        it('should return the whole list of entries', function(done)
-        {
-            t.getEntries(function(err, entries)
-            {
+        it('should return the whole list of entries', function (done) {
+            t.getEntries(function (err, entries) {
                 test.must(err).be.equal(null);
 
                 entries.must.have.length(5);
@@ -176,60 +155,48 @@ describe('Tag', function() {
             });
         });
 
-        it('should untag the blob successfully', function(done)
-        {
-            b.detachTag(dasTag, function(err)
-            {
+        it('should untag the blob successfully', function (done) {
+            b.detachTag(dasTag, function (err) {
                 test.must(err).be.equal(null);
 
                 done();
             });
         });
 
-        it('should untag the integer successfully', function(done)
-        {
-            i.detachTag(dasTag, function(err)
-            {
+        it('should untag the integer successfully', function (done) {
+            i.detachTag(dasTag, function (err) {
                 test.must(err).be.equal(null);
 
                 done();
             });
         });
 
-        it('should untag the deque successfully', function(done)
-        {
-            q.detachTag(dasTag, function(err)
-            {
+        it('should untag the deque successfully', function (done) {
+            q.detachTag(dasTag, function (err) {
                 test.must(err).be.equal(null);
 
                 done();
             });
         });
 
-        it('should untag the set successfully', function(done)
-        {
-            s.detachTag(dasTag, function(err)
-            {
+        it('should untag the set successfully', function (done) {
+            s.detachTag(dasTag, function (err) {
                 test.must(err).be.equal(null);
 
                 done();
             });
         });
 
-        it('should untag the time series successfully', function(done)
-        {
-            ts.detachTag(dasTag, function(err)
-            {
+        it('should untag the time series successfully', function (done) {
+            ts.detachTag(dasTag, function (err) {
                 test.must(err).be.equal(null);
 
                 done();
             });
         });
 
-        it('should return an empty tag list again', function(done)
-        {
-            t.getEntries(function(err, entries)
-            {
+        it('should return an empty tag list again', function (done) {
+            t.getEntries(function (err, entries) {
                 test.must(err).be.equal(null);
 
                 entries.must.have.length(0);
@@ -238,50 +205,40 @@ describe('Tag', function() {
             });
         });
 
-        it('should remove the blob for tag test', function(done)
-        {
-            b.remove(function(err)
-            {
+        it('should remove the blob for tag test', function (done) {
+            b.remove(function (err) {
                 test.must(err).be.equal(null);
 
                 done();
             });
         });
 
-        it('should remove the integer for tag test', function(done)
-        {
-            i.remove(function(err)
-            {
+        it('should remove the integer for tag test', function (done) {
+            i.remove(function (err) {
                 test.must(err).be.equal(null);
 
                 done();
             });
         });
 
-        it('should remove the deque for tag test', function(done)
-        {
-            q.remove(function(err)
-            {
+        it('should remove the deque for tag test', function (done) {
+            q.remove(function (err) {
                 test.must(err).be.equal(null);
 
                 done();
             });
         });
 
-        it('should remove the set for tag test', function(done)
-        {
-            s.remove(function(err)
-            {
+        it('should remove the set for tag test', function (done) {
+            s.remove(function (err) {
                 test.must(err).be.equal(null);
 
                 done();
             });
         });
 
-        it('should remove the time series for tag test', function(done)
-        {
-            ts.remove(function(err)
-            {
+        it('should remove the time series for tag test', function (done) {
+            ts.remove(function (err) {
                 test.must(err).be.equal(null);
 
                 done();
@@ -290,115 +247,108 @@ describe('Tag', function() {
 
     }); // entries list test
 
-    describe('multiple tags', function() {
-        var tags = [ 'line720_0', 'line720_1' ];
+    describe('multiple tags', function () {
+        var tags = ['line720_0', 'line720_1'];
         var b = null;
         var ts = null;
 
-        before('init', function() {
+        before('init', function (done) {
             b = cluster.blob('blob_tag_test');
-            ts = tags.map(function(t) {
+            ts = tags.map(function (t) {
                 return cluster.tag(t);
             });
+            done();
         });
 
-        it('should return an empty tag list', function() {
-            ts.forEach(function (t)
-            {
-                t.getEntries(function (err, entries)
-                {
+        it('should return an empty tag list', function (done) {
+            var nbOfCallbacks = 0
+            for (var i = 0; i < ts.length; i++) {
+                ts[i].getEntries(function (err, entries) {
                     test.must(err).be.equal(null);
-
                     entries.must.have.length(0);
+
+                    nbOfCallbacks++
+                    if (nbOfCallbacks >= ts.length) {
+                        done()
+                    }
                 });
-            });
+            }
         });
 
-        it('should return false (tag not set) for every tag', function(done)
-        {
-            b.hasTags(tags, function (err, success_count, result)
-            {
+        it('should return false (tag not set) for every tag', function (done) {
+            b.hasTags(tags, function (err, success_count, result) {
                 test.must(err).be.equal(null);
                 test.must(success_count).be.equal(0);
 
-                tags.forEach(function (t)
-                {
-                    test.must(result[t]).be.false();
-                });
+                for (var i = 0; i < tags.length; i++) {
+                    test.must(result[tags[i]]).be.false();
+                };
 
                 done();
             });
         });
 
-        it('should update the blob for tag test', function(done)
-        {
-            b.update(new Buffer('untz'), function(err)
-            {
+        it('should update the blob for tag test', function (done) {
+            b.update(new Buffer('untz'), function (err) {
                 test.must(err).be.equal(null);
 
                 done();
             });
         });
 
-        describe('attachTags', function ()
-        {
-
-            it('should add multiple tags successfully', function(done)
-            {
-                b.attachTags(tags, function(err)
-                {
+        describe('attachTags', function () {
+            it('should add multiple tags successfully', function (done) {
+                b.attachTags(tags, function (err) {
                     test.must(err).be.equal(null);
 
                     done();
                 });
             });
 
-            it('should find the tags', function(done)
-            {
-                tags.forEach(function (tagName)
-                {
-                    b.hasTag(tagName, function(err)
-                    {
-                        test.must(err).be.equal(null);
-                    });
-                });
+            it('should find the tags', function (done) {
+                var nbOfCallbacks = 0
+                for (var i = 0; i < tags.length; i++) {
+                    tagName = tags[i]
 
-                done();
+                    b.hasTag(tagName, function (err) {
+                        test.must(err).be.equal(null);
+                        nbOfCallbacks++
+                        if (nbOfCallbacks >= tags.length) {
+                            done()
+                        }
+                    });
+                }
             });
 
-            it('should return the whole list of entries', function(done)
-            {
-                ts.forEach(function (t)
-                {
-                    t.getEntries(function(err, entries)
-                    {
+            it('should return the whole list of entries', function (done) {
+                var nbOfCallbacks = 0
+                for (var i = 0; i < ts.length; i++) {
+                    ts[i].getEntries(function (err, entries) {
                         test.must(err).be.equal(null);
 
                         entries.must.have.length(1);
 
                         test.must(entries.indexOf('blob_tag_test')).be.gte(0);
+                        nbOfCallbacks++
+                        if (nbOfCallbacks >= ts.length) {
+                            done()
+                        }
                     });
-                });
-
-                done();
+                }
             });
 
         }); // attachTags
 
-        describe('hasTags', function ()
-        {
+        describe('hasTags', function () {
 
-            it('should return true (tag set) for every tag', function(done)
-            {
-                b.hasTags(tags, function (err, success_count, result)
-                {
+            it('should return true (tag set) for every tag', function (done) {
+                b.hasTags(tags, function (err, success_count, result) {
                     test.must(err).be.equal(null);
                     test.must(success_count).be.equal(tags.length);
 
-                    tags.forEach(function (t)
-                    {
-                        test.must(result[t]).be.true();
-                    });
+                    for (var i = 0; i < tags.length; i++) {
+                        test.must(result[tags[i]]).be.true();
+                    };
 
                     done();
                 });
@@ -406,50 +356,44 @@ describe('Tag', function() {
 
         }); // hasTags
 
-        describe('detachTags', function ()
-        {
+        describe('detachTags', function () {
 
-            it('should remove multiple tags successfully', function(done)
-            {
-                b.detachTags(tags, function(err)
-                {
+            it('should remove multiple tags successfully', function (done) {
+                b.detachTags(tags, function (err) {
                     test.must(err).be.equal(null);
 
                     done();
                 });
             });
 
-            it('should return an empty tag list', function(done)
-            {
-                ts.forEach(function (t)
-                {
-                    t.getEntries(function (err, entries)
-                    {
+            it('should return an empty tag list', function (done) {
+                var nbOfCallbacks = 0
+                for (var i = 0; i < ts.length; i++) {
+                    ts[i].getEntries(function (err, entries) {
                         test.must(err).be.equal(null);
 
                         entries.must.have.length(0);
-                    });
-                });
 
-                done();
+                        nbOfCallbacks++
+                        if (nbOfCallbacks >= ts.length) {
+                            done()
+                        }
+                    });
+                };
             });
 
         }); // detachTags
 
-        describe('hasTags', function ()
-        {
+        describe('hasTags', function () {
 
-            it('should return false (tag not set) for every tag', function(done)
-            {
-                b.hasTags(tags, function (err, success_count, result)
-                {
+            it('should return false (tag not set) for every tag', function (done) {
+                b.hasTags(tags, function (err, success_count, result) {
                     test.must(err).be.equal(null);
                     test.must(success_count).be.equal(0);
 
-                    tags.forEach(function (t)
-                    {
-                        test.must(result[t]).be.false();
-                    });
+                    for (var i = 0; i < tags.length; i++) {
+                        test.must(result[tags[i]]).be.false();
+                    };
 
                     done();
                 });
