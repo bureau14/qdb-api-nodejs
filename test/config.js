ebeb90546b63ddde435697f9295e283e8440444d
Edouard A <edouard@quasardb.net>
2019-01-05 13:01:48 -0500

    QDB-2300 - Fix Node.JS tests

--- test/config.js
+++ test/config.js
@@ -2,6 +2,6 @@ var address = "127.0.0.1:3030";
 
 module.exports = {
     "qdbd_path" : __dirname + '/../deps/qdb/bin/qdbd',
-    "qdbd_args" : [ '--security=false', '--address=' + address, '--transient' ],
+    "qdbd_args" : [ '--security=false', '--address=' + address, '--storage-engine=transient' ],
     "cluster_uri" : "qdb://" + address
 };
