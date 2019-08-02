a83bf7a84b656229e4edfe77caae0c669ec0abc5
vianney <vianney.p@outlook.com>
2019-08-02 19:05:45 +0200

    Add qdb-test-setup submodule.

--- test/queryTest.js
+++ test/queryTest.js
@@ -2,7 +2,7 @@ var test = require('unit.js');
 var qdb = require('..');
 var config = require('./config');
 
-var cluster = new qdb.Cluster(config.cluster_uri);
+var cluster = new qdb.Cluster(config.insecure_cluster_uri);
 var my_tag1 = 'my_tag1';
 var my_tag2 = 'my_tag2';
 var blob_name = 'blob_query_test';
