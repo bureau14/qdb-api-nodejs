818676571ee85336507e59b06b5b5d7a716d13bb
Leon Mergen <leon@solatis.com>
2021-10-01 16:18:36 +0200

    Better way to fix the same fix

--- scripts/teamcity/20.build.sh
+++ scripts/teamcity/20.build.sh
@@ -5,5 +5,5 @@ set -eux
 SCRIPT_DIR="$(cd "$(dirname -- "${BASH_SOURCE[0]}")" >/dev/null && pwd)"
 source "$SCRIPT_DIR/configure.sh"
 
-rm -rf ~/.npm/_cacache/
+$NPM cache clean --force
 $NPM install --build-from-source --copy_c_api=yes
