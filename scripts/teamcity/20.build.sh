5051c0bb21f489dc1b6f9944a619567de87a23e8
Leon Mergen <leon@solatis.com>
2021-10-01 17:07:02 +0200

    Fix unstable cached dependencies issue

--- scripts/teamcity/20.build.sh
+++ scripts/teamcity/20.build.sh
@@ -5,4 +5,5 @@ set -eux
 SCRIPT_DIR="$(cd "$(dirname -- "${BASH_SOURCE[0]}")" >/dev/null && pwd)"
 source "$SCRIPT_DIR/configure.sh"
 
+$NPM cache clean --force
 $NPM install --build-from-source --copy_c_api=yes
