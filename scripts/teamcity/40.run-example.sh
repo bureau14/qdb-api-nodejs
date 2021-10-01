51d0b37717754fbf293c1357a2e028656e184b8a
Leon Mergen <leon@solatis.com>
2021-10-01 17:08:17 +0200

    Clean cached dependencies before installing example as well

--- scripts/teamcity/40.run-example.sh
+++ scripts/teamcity/40.run-example.sh
@@ -7,5 +7,6 @@ source "$SCRIPT_DIR/configure.sh"
 
 QDB_DIR=`pwd`/qdb
 cd examples/tutorial
+$NPM cache clean --force
 $NPM install --save bureau14/qdb-api-nodejs#${BUILD_BRANCH} --build-from-source --copy_c_api=yes --c_api_path=$QDB_DIR
 $NPM start
