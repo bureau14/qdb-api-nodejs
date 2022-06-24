325bc31bc19cf0801123c3115ea6f3b93f4b2685
Marek Kurdej <marek@quasardb.net>
2022-06-24 11:38:09 +0200

    Verbose build.

--- scripts/teamcity/20.build.sh
+++ scripts/teamcity/20.build.sh
@@ -5,4 +5,4 @@ set -eux
 SCRIPT_DIR="$(cd "$(dirname -- "${BASH_SOURCE[0]}")" >/dev/null && pwd)"
 source "$SCRIPT_DIR/configure.sh"
 
-$NPM install --build-from-source --copy_c_api=yes
+$NPM install --build-from-source --copy_c_api=yes --verbose
